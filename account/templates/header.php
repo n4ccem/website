61618a5fc656348f8b8a1cad1568dbcad83b9bd3
Nassim Chami <74711730+n4ccem@users.noreply.github.com>
2021-09-04 23:54:49 +0100

    Create header.php

---
+++ account/templates/header.php
@@ -0,0 +1,17 @@
+<head>
+    <link rel="stylesheet" href="css/headeer.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/settiing.css">
+
+    <meta charset="utf-8">
+    <title>Welcome</title>
+</head>
+<body style="background-color: #eaeded; padding: 0;margin: 0;">
+    <header class="header">
+        <a class="logo" href="index.php">CINEMA | WORLD</a>
+        <ul class="ul">
+            <li><a class="link" href="./items.php">Items</a></li>
+            <li><a class="link" href="./settings.php">Account Setting</a></li>
+            <li><a class="link" href="logout.php">Logout</a></li>
+        </ul>
+    </header>
