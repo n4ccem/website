a39872c891a7601d39f10db76214afe1f859b19a
Nassim Chami <74711730+n4ccem@users.noreply.github.com>
2021-09-04 23:49:06 +0100

    Update header.php

--- templates/header.php
+++ templates/header.php
@@ -1,1 +1,21 @@
+<head>
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/logiin.css">
+    <link rel="stylesheet" href="css/register.css">
+    <link rel="stylesheet" href="css/index.css">
 
+
+    <meta charset="utf-8">
+    <title>Login Page </title>
+</head>
+<body style="background-color: #eaeded; padding: 0;margin: 0;">
+    <header class="header">
+        <a class="logo" href="index.php">CINEMA | WORLD</a>
+        <ul class="ul">
+            <li><a class="link" href="#">About</a></li>
+            <li><a class="link" href="#">Items</a></li>
+            <li><a class="link" href="login.php">Login</a></li>
+            <li><a class="link" href="register.php">Register</a></li>
+        </ul>
+    </header>
