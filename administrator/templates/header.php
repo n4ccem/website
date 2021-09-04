fdec4a92dcdd7239b821927be70ccb0929cb23f8
Nassim Chami <74711730+n4ccem@users.noreply.github.com>
2021-09-04 23:59:10 +0100

    Create header.php

---
+++ administrator/templates/header.php
@@ -0,0 +1,18 @@
+<head>
+    <link rel="stylesheet" href="css/headeer.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/logiin.css">
+    
+
+
+    <meta charset="utf-8">
+    <title>Login Page </title>
+</head>
+<body style="background-color: white; padding: 0;margin: 0;">
+    <header class="header">
+        <a class="logo" href="index.php">ADMIN PANEL</a>
+        <ul class="ul">
+            <li><a class="link" href="login.php">Login</a></li>
+
+        </ul>
+    </header>
